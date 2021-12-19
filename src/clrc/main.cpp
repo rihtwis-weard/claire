4c40b16dce3d6367c8bec82b3101870a65627bce
Andrew Hu <andrew.hu@pithecine.com>
2021-12-19 18:59:48 -0500

    fix state machine edge cases

--- src/clrc/main.cpp
+++ src/clrc/main.cpp
@@ -3,7 +3,7 @@
 #include "lexer.hpp"
 
 int main(int argc, char const *argv[]) {
-  auto tokens = claire::Lexer{"../../examples/fib.clr"}.lex();
+  auto tokens = claire::Lexer{"../../examples/hello_world.clr"}.lex();
   for (auto const &tok : tokens) {
     std::cout << tok << "\n";
   }
