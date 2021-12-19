37d520da162d20cb34eb351298a7fa8d376129dd
Andrew Hu <andrew.hu@pithecine.com>
2021-12-19 14:06:26 -0500

    add approvals

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -8,4 +8,14 @@ int main() {
   using namespace ApprovalTests;
 
   auto directory = Approvals::useApprovalsSubdirectory("golden_files");
+
+  "hello_world"_test = []() {
+    auto lexemes = claire::Lexer{"../../examples/hello_world.clr"}.lex();
+    Approvals::verifyAll("hello_world.clr", lexemes);
+  };
+
+  "fib"_test = []() {
+    auto lexemes = claire::Lexer{"../../examples/fib.clr"}.lex();
+    Approvals::verifyAll("fib.clr", lexemes);
+  };
 }
